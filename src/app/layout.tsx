fdaaf6fd1f34bc952e5b698fa99e28ae50ab6424
Surya <cssurya2006@gmail.com>
2026-05-30 19:28:07 +0530

    final fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     images: ['/images/surya-portrait.jpg'],
   },
   verification: {
-    google: ['nzjJHjv3TTj8mUzNEFx4RH3ecj-OGGnMglPAKj3IubQ', 'FXp4jqzCy6bxO6ThBXz34F29BgX2w0qgt9v4tONOfec'],
+    google: ['FXp4jqzCy6bxO6ThBXz34F29BgX2w0qgt9v4tONOfec'],
   },
 };
 
